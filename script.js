2629ed9dfacec80039c06d5bf285f25234e611b2
Joseph Pollock <josephgp.0458@gmail.com>
2018-01-15 07:21:33 +1000

    Got the minimum functionality outlined in the README.md

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
-let gridSize = 16 * 16;
+let enteredSize = 16;
 
-for (let i = 0; i < gridSize; i++) {
-    let gridSquare = document.createElement("div");
-    gridSquare.setAttribute("id", "gridSquare");
-    container.appendChild(gridSquare);
-    gridSquare.addEventListener('mouseover', function() {
-      gridSquare.className = ("colored");
-    });
+function buildGrid() {
+  let gridSize = enteredSize * enteredSize;
+  let gridSquareSize = (500 / enteredSize) + "px";
+  for (let i = 0; i < gridSize; i++) {
+      let gridSquare = document.createElement("div");
+      gridSquare.setAttribute("id", "gridSquare");
+      container.appendChild(gridSquare);
+      gridSquare.style.height = gridSquareSize;
+      gridSquare.style.width = gridSquareSize;
+      gridSquare.addEventListener('mouseover', function() {
+        gridSquare.classList.add("colored");
+      });
+  }
 }
+
+buildGrid();
+
+let reset = document.querySelector("#reset");
+reset.addEventListener("click", function(e) {
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  };
+  enteredSize = prompt("Enter new grid size: ");
+  buildGrid();
+});
